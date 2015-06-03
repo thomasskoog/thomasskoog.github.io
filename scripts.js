e22366d0d8ef8c4b7cc2bdd0bc0e8a79cfa6ddb0
Thomas Skoog <thomas.skoog@gmail.com>
2015-06-03 14:34:06 +0200

    bantade ner knappkoden med switches

--- scripts.js
+++ scripts.js
@@ -1,27 +1,30 @@
 /**
  * Created by Thomas on 19/05/2015.
  */
+
+//Hoppa mellan sidinnehåll
+
+document.addEventListener("click", function(e){
+   var valueButton = e.path[1].value;
+   switch (valueButton){
+       case 0:
+           document.getElementById("articleOm").style.display = "";
+           document.getElementById("articlePortfolio").style.display = "none";
+           document.getElementById("articleKontakt").style.display = "none";
+           break;
+       case 1:
+           document.getElementById("articleOm").style.display = "none";
+           document.getElementById("articlePortfolio").style.display = "";
+           document.getElementById("articleKontakt").style.display = "none";
+           break;
+       case 2:
+           document.getElementById("articleOm").style.display = "none";
+           document.getElementById("articlePortfolio").style.display = "none";
+           document.getElementById("articleKontakt").style.display = "";
+           break;
+   }
+});
+
 document.getElementById("articleOm").style.display = "none";
 document.getElementById("articlePortfolio").style.display = "";
 document.getElementById("articleKontakt").style.display = "none";
-
-document.getElementsByClassName("buttonNav")[0].addEventListener("click", function(){
-    document.getElementById("articleOm").style.display = "";
-    document.getElementById("articlePortfolio").style.display = "none";
-    document.getElementById("articleKontakt").style.display = "none";
-    document.getElementById("codeImg").style.display = "none";
-});
-
-document.getElementsByClassName("buttonNav")[1].addEventListener("click", function(){
-    document.getElementById("articleOm").style.display = "none";
-    document.getElementById("articlePortfolio").style.display = "";
-    document.getElementById("articleKontakt").style.display = "none";
-    document.getElementById("codeImg").style.display = "none";
-});
-
-document.getElementsByClassName("buttonNav")[2].addEventListener("click", function(){
-    document.getElementById("articleOm").style.display = "none";
-    document.getElementById("articlePortfolio").style.display = "none";
-    document.getElementById("articleKontakt").style.display = "";
-    document.getElementById("codeImg").style.display = "none";
-});
