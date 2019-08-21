080aa9d6d33430fda7d73bbb90ce797d84a25db8
Thomas Skoog <thomas.skoog@gmail.com>
2019-08-21 13:00:27 +0200

    testing adding react to website

---
+++ app.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'React';
+'use strict';
+
+const e = React.createElement;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#app');
+reactDOM.render(e(App), domContainer);
